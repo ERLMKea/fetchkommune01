8fea7b5448fd3939d6b3473cb4f97b1e77a6389f
ERLMKea <erlm@kea.dk>
2022-02-25 09:50:35 +0100

    read json, create map of kommuner

--- js/fetchkommuner.js
+++ js/fetchkommuner.js
@@ -9,6 +9,12 @@ const kommuneUrl = 'https://api.dataforsyningen.dk/kommuner';
 function getAllKommuner() {
   out("get all kommuner kaldt");
   //call fetch, when fetch returns, call then()
+  //response.json er også en async function
+  //return fetch(kommuneUrl).then(response => {
+  //  out("response obj");
+  //  out(response);
+  //  response.json();
+  //}); //ovenstående returnere ikke noget brugbart
   return fetch(kommuneUrl).then(response => response.json());
 }
 
@@ -17,18 +23,33 @@ function callGetAllKommuner() {
   out(promise);
 }
 
+const kommuneMap = new Map();
 async function showAllKommuner() {
   out("show allkommuner");
   const kommuneList = await getAllKommuner();
+  out(kommuneList);
   kommuneList.forEach((kommune, index) => {
     out(kommune.navn + "ix=" + index);
+    kommuneMap.set(kommune.navn, kommune);
   })
+}
+
+function showKommuneMap() {
+  for (const komKey of kommuneMap.keys()) {
+    out(kommuneMap.get(komKey));
+  }
 }
 
 //callGetAllKommuner();
 showAllKommuner();
+showKommuneMap();
 
+const pbGetKommuner = document.getElementById('pbGetKommune');
+const pbShowKommuneMap = document.getElementById('pbShowKommuneMap');
 
 out("vi er færdige her");
+
+pbGetKommuner.addEventListener('click', showAllKommuner);
+pbShowKommuneMap.addEventListener('click', showKommuneMap);
 
 
